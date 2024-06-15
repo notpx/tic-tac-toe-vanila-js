dafcc50b0996e9c8552d3a46d5e85ed9da650114
Bob <bob@example.com>
2024-06-15 14:31:52 +0530

    made multiplayer work

--- app.js
+++ app.js
@@ -1,61 +1,32 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
 
-    var turn = true;
-    var final;
-    function winnerChecker() {
-        var cells = [[document.getElementById('one').innerHTML, document.getElementById('two').innerHTML, document.getElementById('three').innerHTML],
-    [document.getElementById('four').innerHTML, document.getElementById('five').innerHTML, document.getElementById('six').innerHTML],
-    [document.getElementById('seven').innerHTML, document.getElementById('eigth').innerHTML, document.getElementById('nine').innerHTML]];
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-    //checking for each row 
-    for (var i = 0; i < 3; i++) {
-            if (cells[i][0] == cells[i][1] && cells[i][1] == cells[i][2]) {
-                return document.getElementById('win').innerHTML = "winner is" + cells[i][0];
-    final = true;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-            }
+io.on('connection', (socket) => {
+    console.log('a user connected');
 
-        }
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 
-    //checking for column
-    for (var j = 0; j < 3; j++) {
-            if (cells[0][j] == cells[1][j] && cells[1][j] == cells[2][j]) {
-                return document.getElementById('win').innerHTML = "winner is" + cells[0][j];
-    final = true;
-            }
-        }
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
-    var z = 0;
-    var rz = 2;
-    if (cells[z][z] == cells[z + 1][z + 1] && cells[z + 1][z + 1] == cells[z + 2][z + 2]) {
-            return document.getElementById('win').innerHTML = "winner is" + cells[z][z];
-    final = true;
-        }
-
-
-    if (cells[0][2] == cells[1][1] && cells[1][1] == cells[2][0]) {
-                return document.getElementById('win').innerHTML = "winner is" + cells[0][2];
-    final = true;
-            }
-
-
-    if (final)  {
-    
-        for(var k = 0; k<3; k++){
-        cells[k][k + 1] = "Z";   
-        }
-
-
-    }
-}
-
-    function replacer(id) {
-        if (document.getElementById(id).innerHTML !== "") {
-        document.getElementById(id).innerHTML = turn ? 'O' : 'X';
-    turn = !turn;
-    winnerChecker();
-
-        }
-
-    }
-
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
